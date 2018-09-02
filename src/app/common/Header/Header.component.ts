d7e87c56f454944db9d17898e4ec960e27b11371
Vivek Ravikumar <vr329@njit.edu>
2018-09-02 16:20:39 -0700

    Header component and scss added

---
+++ src/app/common/Header/Header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "bwm-header",
+  templateUrl: "./Header.component.html",
+  styleUrls: ["./Header.component.scss"]
+})
+export class HeaderComponent {}
